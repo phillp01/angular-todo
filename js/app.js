185fb2494d4d669eebfcb2a64669269f05649e4b
Paul <paul@kingofthemoon.co.uk>
2016-10-20 20:31:14 +0100

    Added Name and password fields to the Registration form.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+angular.module('TodoApp',['ngRoute','RouteControllers']);
+
+angular.module('TodoApp').config(function($routeProvider)	{
+
+	$routeProvider.when('/',	{
+		templateUrl:'templates/home.html',
+		controller:'HomeController'
+	})
+	.when('/accounts/register',	{
+		templateUrl:'templates/register.html',
+		controller:'RegisterController'
+	});
+});
