a86742b2a584a9984e2abe2d1f1b97b756b00d27
Paul <paul@kingofthemoon.co.uk>
2016-10-28 18:14:31 +0100

    Problem Solving

--- js/service.js
+++ js/service.js
@@ -9,17 +9,18 @@ angular.module('UserService',[])
 		return UserAPIService;
 	});
 
-angular.module('TodoService', [])
+angular.module('TodoService',[])
 	.factory('TodoAPIService', function($http)	{
-		TodoAPIService = {
-			getTodos: function(url,data,token)	{
-				header = "Authorization: JWT " + token;
-				return $http.get(url, {params:{"username": data}}, header);
-			},
-			createTodo: function(url, data, token)	{
-				header = "Authorization : JWT " + token;
-				return $http.post(url, data, header);
-			}
+		
+		var TodoAPIService = {
+			// getTodos: function(url, data, token)	{
+			// 	header = "Authorization: JWT " + token;
+			// 	return $http.get(url, {params:{"username": data}}, header);
+			// },
+			// createTodo: function(url, data, token)	{
+			// 	header = "Authorization: JWT " + token;
+			// 	return $http.post(url, data, header);
+			// }
 		};
-		return TodoAPIService;
+		//return TodoAPIService;
 	});
