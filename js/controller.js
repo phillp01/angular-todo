50eb51c7128647815f8b8dab8a252dae4b8b5380
Paul <paul@kingofthemoon.co.uk>
2016-10-29 20:57:50 +0100

    Corrected model error in todo.html

--- js/controller.js
+++ js/controller.js
@@ -21,6 +21,7 @@ angular.module('RouteControllers', [])
 							$scope.token = results.data.token;
 							store.set('username', $scope.registrationUser.username);
 							store.set('authToken', $scope.token);
+							console.log($scope.token);
 						}).catch(function(err)	{
 							console.log(err);
 						});
@@ -33,29 +34,28 @@ angular.module('RouteControllers', [])
 	})
 	.controller('TodoController', function($scope, $location, TodoAPIService, store)	{
 		var URL = "https://morning-castle-91468.herokuapp.com/";
-		console.log("HELLO");
+		
 		$scope.authToken = store.get('authToken');
 		$scope.username = store.get('username');
-
+	
 		$scope.todos = [];
 
-		// TodoAPIService.getTodos(URL + "todo/", $scope.username, $scope.authToken).then(function(results)	{
-		// 	$scope.todos = results.data;
-		// 	console.log($scope.todos);
-		// }).catch(function(err)	{
-		// 	console.log(err);
-		// });
+		TodoAPIService.getTodos(URL + "todo/", $scope.username, $scope.authToken).then(function(results)	{
+			$scope.todos = results.data;
+		}).catch(function(err)	{
+			console.log(err);
+		});
 
 		$scope.submitForm = function()	{
 			if ($scope.todoForm.$valid)	{
 				$scope.todo.username = $scope.username;
 				$scope.todos.push($scope.todo);
 
-				// TodoAPIService.createTodo(URL + "todo/", $scope.todo, $scope.authToken).then(function(results)	{
-				// 	console.log(results);
-				// }).catch(function(err)	{
-				// 	console.log(err);
-				// });
+				TodoAPIService.createTodo(URL + "todo/", $scope.todo, $scope.authToken).then(function(results)	{
+					console.log(results);
+				}).catch(function(err)	{
+					console.log(err);
+				});
 			}
 		}
 	});
--- js/service.js
+++ js/service.js
@@ -11,16 +11,16 @@ angular.module('UserService',[])
 
 angular.module('TodoService',[])
 	.factory('TodoAPIService', function($http)	{
-		
 		var TodoAPIService = {
-			// getTodos: function(url, data, token)	{
-			// 	header = "Authorization: JWT " + token;
-			// 	return $http.get(url, {params:{"username": data}}, header);
-			// },
-			// createTodo: function(url, data, token)	{
-			// 	header = "Authorization: JWT " + token;
-			// 	return $http.post(url, data, header);
-			// }
+			
+			getTodos: function(url, data, token)	{
+				header = "Authorization: JWT " + token;
+				return $http.get(url, {params:{"username": data}}, header);
+			},
+			createTodo: function(url, data, token)	{
+				header = "Authorization: JWT " + token;
+				return $http.post(url, data, header);
+			}
 		};
-		//return TodoAPIService;
+		return TodoAPIService;
 	});
